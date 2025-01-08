36a76cc40ad5ee189d4b865dd5e97f7ab7c48c7b
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-01-08 08:53:19 -0500

    fcc react 19 section 2 lesson 12 destructuring props object

--- props/src/Contact.jsx
+++ props/src/Contact.jsx
@@ -1,27 +1,37 @@
-export default function Contact(props){
+export default function Contact({pfp,phone,namephone,email}){
     return(
         <article className="contact-card">
             <img
                 className="pfp"
-                src={props.pfp}
+                src={pfp}
                 alt="Photo of {props.name}"
             />
-            <h3>{props.name}</h3>
+            <h3>{name}</h3>
             <div className="info-group">
             <img
                 src="./phone-icon.png"
                 alt="phone icon"
             />
-            <p>{props.phone}</p>
+            <p>{phone}</p>
             </div>
             <div className="info-group">
             <img
                 src="./email-icon.png"
                 alt="email icon"
             />
-            <p>{props.email}</p>
+            <p>{email}</p>
             </div>
         </article>
 
     )
 }
+
+const person = {
+    img: "./mr-whiskerson.jpg",
+    name: 'Mr. Whiskerson',
+    phone:'(800) 555-1234',
+    email:'mr.whiskaz@catnap.meow'
+}
+
+const {img, name} = person
+console.log(img)
